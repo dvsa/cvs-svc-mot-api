258096d3150519754942930dfb4f16206d5ef3ad
Melanie Coslett <melanie.coslett@dvsa.gov.uk>
2023-09-15 10:44:08 +0100

    feat(vta-1575): welsh certificate for trailer pass

--- src/main/java/uk/gov/dvsa/model/cvs/VTG5AW.java
+++ src/main/java/uk/gov/dvsa/model/cvs/VTG5AW.java
@@ -3,7 +3,7 @@ package uk.gov.dvsa.model.cvs;
 import uk.gov.dvsa.model.Document;
 import uk.gov.dvsa.model.cvs.certificateData.CvsMotCertificateData;
 
-public class VTG5A extends CvsMotCertificate {
+public class VTG5AW extends CvsMotCertificate {
 
     public Document setData(CvsMotCertificateData data) {
         this.data = data;
@@ -15,12 +15,12 @@ public class VTG5A extends CvsMotCertificate {
     }
 
     public String getPresentedDocumentNamePass() {
-        return "VTG5A";
+        return "VTG5AW";
     }
 
     public String getVersionNumberPass() {
         return "1.0";
     }
 
-    public String getRegOrIdHeading() { return "Identification number";  }
+    public String getRegOrIdHeading() { return "Rhif cofrestru";  }
 }
