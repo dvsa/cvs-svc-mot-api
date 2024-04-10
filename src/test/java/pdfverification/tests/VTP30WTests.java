8998b8c23bfce0db7986ae3d717beba711dcd940
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2024-04-10 10:57:55 +0100

    feat(CB2-9306): comments

--- src/test/java/pdfverification/tests/VTP30WTests.java
+++ src/test/java/pdfverification/tests/VTP30WTests.java
@@ -10,6 +10,8 @@ import pdfverification.service.PDFParser;
 import uk.gov.dvsa.model.cvs.VTP30W;
 import uk.gov.dvsa.service.HtmlGenerator;
 import uk.gov.dvsa.service.PDFGenerationService;
+
+import java.io.FileOutputStream;
 import java.io.IOException;
 import static htmlverification.framework.component.DefectSummaryComponent.*;
 import static org.junit.Assert.assertTrue;
