9b71a398080e4af093153a850a080fad25f1e62d
Craig Broady <Craig.Broady@dvsa.gov.uk>
2024-04-17 09:03:25 +0100

    feat(cb2-9315): added unit tests and removed logger

--- src/test/java/pdfverification/tests/HgvPRSBilingualTests.java
+++ src/test/java/pdfverification/tests/HgvPRSBilingualTests.java
@@ -15,19 +15,21 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 
-public class VTG30WTests {
-    private static final String CERT_NAME = "Gwrthod tystysgrif prawf MOT";
+public class HgvPRSBilingualTests {
+    private static final String CERT_NAME = "MOT test certificate (HGV)";
+    private static final String CERT_NAME_FAIL = "Refusal of MOT test certificate";
+    private static final String CERT_NAME_WELSH = "Tystysgrif prawf MOT (HGV)";
+    private static final String CERT_NAME_FAIL_WELSH = "Gwrthod tystysgrif prawf MOT";
 
     private HtmlGenerator htmlGenerator;
     private PDFGenerationService pdfGenerationService;
     private CvsMotCertificate testCertificate;
     private PDFParser pdfParser;
-
     private PdfReader pdfReader;
     private byte[] pdfData;
 
-    public VTG30WTests() {
-        this.testCertificate = CvsCertificateTestDataProvider.getVTG30W();
+    public HgvPRSBilingualTests() {
+        this.testCertificate = CvsCertificateTestDataProvider.getCvsHgvPRSBilingual();
         this.htmlGenerator = new HtmlGenerator(new Handlebars());
         this.pdfGenerationService = new PDFGenerationService(new ITextRenderer());
         this.pdfParser = new PDFParser();
@@ -42,11 +44,14 @@ public class VTG30WTests {
     @Test
     public void verifyTitle() throws IOException {
         assertTrue(pdfParser.getRawText(pdfReader, 1).contains(CERT_NAME));
+        assertTrue(pdfParser.getRawText(pdfReader, 2).contains(CERT_NAME_FAIL));
+        assertTrue(pdfParser.getRawText(pdfReader, 3).contains(CERT_NAME_WELSH));
+        assertTrue(pdfParser.getRawText(pdfReader, 4).contains(CERT_NAME_FAIL_WELSH));
     }
 
     @Test
     public void verifySinglePageWithInvalidXMLCharacter() throws Exception {
-        this.testCertificate = CvsCertificateTestDataProvider.getVTG30WHavingInvalidXMLCharacter();
+        this.testCertificate = CvsCertificateTestDataProvider.getCvsHgvPRSBilingualHavingInvalidXMLCharacter();
         pdfParser.getRawText(pdfReader, 1);
     }
 }
