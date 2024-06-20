979c3ba19bea9636e8b4b2479e8e919bb7914afe
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2024-06-20 14:06:05 +0100

    feat(CB2-12602): spelling correction

--- src/test/java/htmlverification/tests/IVA30Test.java
+++ src/test/java/htmlverification/tests/IVA30Test.java
@@ -89,7 +89,7 @@ public class IVA30Test {
     @Test
     public void verifyReapplicationDate() {
         String reapplicationDate = ivaPageObject.getReapplicationDate();
-        assertEquals("Reapplication required by:", reapplicationDate);
+        assertEquals("Reapplication required by: 27/05/2024", reapplicationDate);
     }
 
     @Test
